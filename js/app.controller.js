9af52b33b3bc11f294d0c2c0a06830974f95a9ad
gertzMan <101058145+gertzMan@users.noreply.github.com>
2023-05-03 23:07:55 +0300

    main layout and functionality completed

--- js/app.controller.js
+++ js/app.controller.js
@@ -6,66 +6,120 @@ window.onAddMarker = onAddMarker
 window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
+window.onRemoveLoc = onRemoveLoc
+window.getWeather = locService.getWeather
+window.onSearchLoc = onSearchLoc
 
 function onInit() {
-	mapService
-		.initMap()
-		.then((map) => {
-			console.log('Map is ready')
-			map.addListener('click', ev => {
-				const name = prompt('Place name?', 'Place 1')
-				const lat = ev.latLng.lat()
-				const lng = ev.latLng.lng()
-				locService.saveLocation({ name, lat, lng })
-				// renderPlaces()
-				// renderMarkers()
-			})
-		})
-		.catch(() => console.log('Error: cannot init map'))
+  mapService
+    .initMap()
+    .then((map) => {
+      console.log('Map is ready')
+      map.addListener('click', (ev) => {
+        const name = prompt('Place name?', 'Place 1')
+        const lat = ev.latLng.lat()
+        const lng = ev.latLng.lng()
+        locService.saveLocation({ name, lat, lng }).then(() => {
+          if (!isTableHidden()) {
+            onGetLocs()
+          }
+        })
+      })
+    })
+    .catch(() => console.log('Error: cannot init map'))
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-	console.log('Getting Pos')
-	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject)
-	})
+  console.log('Getting Pos')
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
 }
 
 function onAddMarker() {
-	console.log('Adding a marker')
-	mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+  console.log('Adding a marker')
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
-// render Locations -> add .then(renderMarker(loc))
 function onGetLocs() {
-	locService.getLocs().then((locs) => {
-		console.log('Locations:', locs)
-		document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2)
-		return locs
-	})
-		.then(locs => {
-			locs.forEach(loc =>
-				mapService.addMarker({ lat: loc.lat, lng: loc.lng })
-			)
-		})
+  locService
+    .getLocs()
+    .then((locs) => {
+      console.log('Locations:', locs)
+      showTable()
+      renderLocs(locs)
+      return locs
+    })
+    .then((locs) => {
+      locs.forEach((loc) =>
+        mapService.addMarker({ lat: loc.lat, lng: loc.lng })
+      )
+    })
 }
 
 function onGetUserPos() {
-	getPosition()
-		.then((pos) => {
-			console.log('User position is:', pos.coords)
-			document.querySelector(
-				'.user-pos'
-			).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-		})
-		.catch((err) => {
-			console.log('err!!!', err)
-		})
+  getPosition()
+    .then((pos) => {
+      console.log('User position is:', pos.coords)
+      document.querySelector(
+        '.user-pos'
+      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+      onPanTo(pos.coords.latitude, pos.coords.longitude)
+    })
+    .catch((err) => {
+      console.log('err!!!', err)
+    })
 }
-function onPanTo() {
-	console.log('Panning the Map')
-	mapService.panTo(35.6895, 139.6917)
+function onPanTo(lat, lng) {
+  console.log('Panning the Map')
+  mapService.panTo(lat, lng)
 }
 
+function onRemoveLoc(id) {
+  locService
+    .removeLocation(id)
+    //   locService.getLocs().then((locs) => renderLocs(locs))
+    .then(() => onGetLocs())
+}
 
+function onSearchLoc() {
+  let txt = document.querySelector('#search-text').value
+  // let coord = locService
+  //   .getLocationCoords(txt)
+  //   .then((res) => getWeather(res.lat,res.lng)).then(res=>)
+  locService
+    .getLocationData(txt)
+    .then((location) => locService.saveLocation(location))
+}
+
+//TODO: complete
+function onPanSearch() {}
+
+//rendering
+
+function renderLocs(locs) {
+  console.log('locs', locs)
+  let strHTML = locs
+    .map(
+      (loc) =>
+        `<tr>
+		  <td>${loc.name}</td>
+		  <td>${loc.lat}</td>
+		  <td>${loc.lng}</td>
+		  <td> <button class='update-btn' onclick="onPanTo(${loc.lat},
+        ${loc.lng})">Go</button></td>
+		  <td> <button class='delete-btn' onclick="onRemoveLoc('${loc.id}')">Delete</button></td>
+		  </tr>    `
+    )
+    .join('')
+  document.querySelector('.locs').innerHTML = strHTML
+}
+
+function showTable() {
+  document.querySelector('.table-container').classList.remove('hidden')
+}
+
+function isTableHidden() {
+  return document.querySelector('.table-container').classList.contains('hidden')
+}
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,67 +2,133 @@ import { utilService } from './util.service.js'
 import { storageService } from './async-storage.service.js'
 
 const STORAGE_KEY = 'locationDB'
+const WEATHER_API_KEY = 'db01c6bc7f06c52f0fd3962192fd547a'
+const MAP_API_KEY = 'AIzaSyCOBwLSYGfXaWo-1PFFb4Pz4Qh3-glhrHA'
 
 export const locService = {
-    saveLocation,
-    removeLocation,
-    getLocs
+  saveLocation,
+  removeLocation,
+  getLocs,
+  getWeather,
+  getLocationCoords,
+  getLocationData,
 }
 
 _createLocations()
 
 function getLocs() {
-    return storageService.query(STORAGE_KEY)
-        .then(locations => {
-            console.log(locations)
-            return locations
-        })
+  return storageService.query(STORAGE_KEY).then((locations) => {
+    console.log(locations)
+    return locations
+  })
 }
 
 function saveLocation(location) {
-    if (location.id) {
-        return storageService.put(STORAGE_KEY, location)
-    } else {
-        location = _createLocation(location.name, location.lat, location.lng)
-        return storageService.post(STORAGE_KEY, location)
-    }
+  if (location.id) {
+    return storageService.put(STORAGE_KEY, location)
+  } else {
+    location = _createLocation(location.name, location.lat, location.lng)
+    return storageService.post(STORAGE_KEY, location)
+  }
 }
-// 
+//
 
 function removeLocation(locationId) {
-    return storageService.remove(STORAGE_KEY, locationId)
+  return storageService.remove(STORAGE_KEY, locationId)
 }
-
 
 // {id, name, lat, lng, weather, createdAt, updatedAt}
 
 // private create location
 function _createLocation(name, lat, lng) {
-    const location = {}
-    location.name = name
-    location.lat = lat
-    location.lng = lng
-    location.createdAt = Date.now()
-    location.updatedAt = Date.now()
-    return location
+  const location = {}
+  location.name = name
+  location.lat = lat
+  location.lng = lng
+  location.createdAt = Date.now()
+  location.updatedAt = Date.now()
+  return location
 }
 
 function _createLocations() {
-    let locations = utilService.loadFromStorage(STORAGE_KEY)
-    if (!locations || !locations.length) {
-        _createDemoLocs()
-    }
+  let locations = utilService.loadFromStorage(STORAGE_KEY)
+  if (!locations || !locations.length) {
+    _createDemoLocs()
+  }
 }
 
 function _createDemoLocs() {
-    const locs = [
-        { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-        { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-    ]
+  const locs = [
+    {
+      name: 'Greatplace',
+      lat: 32.047104,
+      lng: 34.832384,
+      id: utilService.makeId(),
+    },
+    {
+      name: 'Neveragain',
+      lat: 32.047201,
+      lng: 34.832581,
+      id: utilService.makeId(),
+    },
+  ]
 
-    utilService.saveToStorage(STORAGE_KEY, locs)
+  utilService.saveToStorage(STORAGE_KEY, locs)
 }
 
+function getWeather(lat, lng) {
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}`
+  return axios.get(url).then((res) => console.log('res.data', res.data))
+}
 
+function getLocationCoords(locationString) {
+  const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationString}&key=${MAP_API_KEY}`
 
+  return axios
+    .get(geocodingApiUrl)
+    .then((response) => {
+      if (response.data.results.length === 0) {
+        throw new Error('Invalid location')
+      }
 
+      const { lat, lng } = response.data.results[0].geometry.location
+      return { lat, lng }
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+function getLocationData(locationString) {
+  const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationString}&key=${MAP_API_KEY}`
+
+  return axios.get(geocodingApiUrl).then((response) => {
+    if (response.data.results.length === 0) {
+      throw new Error('Invalid location')
+    }
+
+    const { lat, lng } = response.data.results[0].geometry.location
+    const name = response.data.results[0].formatted_address
+
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${WEATHER_API_KEY}`
+    return axios.get(weatherApiUrl).then((response) => {
+      const { main, description, icon } = response.data.weather[0]
+      const temperature = response.data.main.temp
+      const createdAt = new Date().toISOString()
+
+      const weatherData = {
+        name,
+        lat,
+        lng,
+        weather: {
+          main,
+          description,
+          temperature,
+          icon,
+        },
+        createdAt,
+      }
+      return weatherData
+    })
+  })
+}
